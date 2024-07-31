d0b455924b93791b408f0fbda9eb76b2af15936e
K0baU <63403741+K0baU@users.noreply.github.com>
2024-07-31 20:31:20 +0900

    Parse CSV

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.getElementsByClassName("md").forEach(async elm=>{
-  let content=await fetch(elm.dataset.href).text();
-  elm.innerHTML=content;
+document.getElementsByClassName("load").forEach(async elm=>{
+  const text=await fetch(elm.dataset.href).text();
+  table=document.createElement("table");
+  for(const line of text.split("\n")){
+    const tr=document.createElement("tr");
+    for(const data of line.split("\n")){
+      const td=document.createElement("td");
+      td.textContent=data;
+      tr.append(td);
+    }
+    table.append(tr);
+  }
+  elm.append(table);
 });
