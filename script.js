0941bdbff83b9eeb406c5de5b5a31aaa6873b0b3
K0baU <63403741+K0baU@users.noreply.github.com>
2024-08-01 23:06:08 +0900

    空行をつくらない

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 for(const elm of document.getElementsByClassName("load")){
   fetch(elm.dataset.href).then(res=>res.text()).then(text=>{
     for(const line of text.split("\n")){
+      if(!li)continue;
       const li=document.createElement("li");
       elm.append(li);
       const ol=document.createElement("ol");
