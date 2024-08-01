7a3903f8cb002f0dc4305d866cbee22ca852912b
K0baU <63403741+K0baU@users.noreply.github.com>
2024-08-02 00:32:19 +0900

    書き込み対象を変更、classNameを追加

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-for(const elm of document.getElementsByClassName("load")){
-  fetch(elm.dataset.href).then(res=>res.text()).then(text=>{
+for(const elm of document.getElementsByTagName("main")){
+  fetch(`/data/${location.search||"roadmap"}.csv`).then(res=>res.text()).then(text=>{
+    const table=document.createElement("ol");
+    elm.append(table);
     for(const line of text.split("\n")){
       if(!line)continue;
       const li=document.createElement("li");
-      elm.append(li);
+      table.append(li);
+      li.className="bar";
       const ol=document.createElement("ol");
       li.append(ol);
+      ol.className="row";
       for(const data of line.split(",")){
         const lili=document.createElement("li");
         ol.append(lili);
         lili.textContent=data;
+        lili.className="data";
       }
     }
   });
